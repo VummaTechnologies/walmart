b9898997197fdba87b4f6bd0c10d90010869358e
sridharvumma <sridhar6358@gmail.com>
2021-07-30 00:15:10 +0530

    committed directly

--- Users.java
+++ Users.java
@@ -1,4 +1,4 @@
 This file contains users info 
 Modifying the file again and uploading it
 Dont have to add to staging area can commit to 
-the local repository directly
+the local repository directly.
